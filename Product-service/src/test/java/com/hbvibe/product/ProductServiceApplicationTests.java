7c9aba43d9934d21f6fece7da769a3ad2f7ee11f
chitieu <hungbeu292005@gmail.com>
2026-08-22 17:00:01 +0700

    22/8 brand-service add method add member brand

---
+++ Product-service/src/test/java/com/hbvibe/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbvibe.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
